7ea1d591481e4f8fecea41cf804066acdf898d3b
balwantrawat777 <balwantrawat333@gmail.com>
2018-06-13 20:19:05 +0530

    assignment is complete

---
+++ assignment2.py
@@ -0,0 +1,34 @@
+#Question 1
+
+print("world is beautiful")
+
+
+#Question 2
+
+print("hello"+"world")
+
+
+#Question 3
+
+a=input("enter a")
+b=input("enter b")
+c=input("enter c")
+print(a,b,c)
+
+
+#Question 4
+
+print("let's get started")
+
+
+#Question 5
+s="acadview"
+course="python"
+fee=5000
+print("%s %s %d" %(s,course,fee))
+
+
+#Question 6.
+name="tony stark"
+salary=1000000
+print('%s' ' %d'%(name,salary))
